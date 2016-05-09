a697ef61f1c2d147cb4ad85443ade75a6e0b9549
sciornei <ciornei-stefan.msn@hotmail.ca>
2016-05-09 11:50:10 -0400

    les pages

--- index.php
+++ index.php
@@ -5,17 +5,11 @@
 <?php require_once ("views/page_top.php"); ?>
 <?php require_once ("views/header.php"); ?>
 <div id="main">
-/*CODE HTML*/
-<?php /*Affichage du catalogue*/ ?>
-<ul>
-<?php foreach ($data as $id => $items) { ?>
-    <li>
-        <div>
-            <p><?php $item["nom"] ?>, <span class=".prix"></span></p>
-        </div>
-    </li>
-<?php } ?>
-</ul>
-</div>
 
+
+
+
+
+</div>
+<?php require_once ("views/footer.php"); ?>
 <?php require_once ("views/page_bottom.php.php"); ?>
