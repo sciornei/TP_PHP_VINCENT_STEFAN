6bdc4a218cd0731e77292e3e947347fdc6df19ca
sciornei <ciornei-stefan.msn@hotmail.ca>
2016-05-10 18:27:48 -0400

    Add files via upload
    dossiers avec produits (1produit/page)

---
+++ produits/darksouls3.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+
+<?php require_once("views/page_top.php"); ?>
+<?php require_once("views/header.php"); ?>
+<div id="main">
+
+    <div><img src="darksouls3_ps4.jpg" alt="DarkSouls3"</div>
+    <p>DESCRIPTION ICI</p>
+    <p>PRIX ICI</p>
+
+
+</div>
+<?php require_once("views/footer.php"); ?>
+<?php require_once("views/page_bottom.php.php"); ?>
---
+++ produits/fallout4.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+
+<?php require_once("views/page_top.php"); ?>
+<?php require_once("views/header.php"); ?>
+<div id="main">
+
+
+
+
+
+</div>
+<?php require_once("views/footer.php"); ?>
+<?php require_once ("views/page_bottom.php.php"); ?>
