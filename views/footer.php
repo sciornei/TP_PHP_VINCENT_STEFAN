30acc6db394130b0b1179ee73c32bd7ec983dc28
xteye <vincexteye@hotmail.fr>
2016-05-11 08:57:19 -0400

    logo

--- views/footer.php
+++ views/footer.php
@@ -5,7 +5,7 @@
  <div id="footer">
 <p id="copy-rights">&copy;2016 Tous droits révervés</p>
 <p id="site-web">www.vsgames.com</p>
-<a href="https://www.facebook.com/zuck"><img src="image/logo/facebook-icon.png" id="logo-fb" alt="logo facebook"/></a>
+<a href="https://www.facebook.com/zuck"><img src="image/logo/facebook-icon.png" id="logo-facebook" alt="logo facebook"/></a>
 <a href="https://twitter.com/pewdiepie"><img src="image/logo/twitter-Logo.png" id="logo-twitter" alt="logo twitter"/></a>
 <a href="https://www.instagram.com/rondarousey/"> <img src="image/logo/instagram-icon.png" id="logo-insta" alt="logo instagram"/></a>
 </div>
