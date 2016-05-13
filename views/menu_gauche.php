e9630d165ca0ef33f8292028092228d7c593cabb
xteye <vincexteye@hotmail.fr>
2016-05-13 11:53:20 -0400

    Add files via upload

---
+++ views/menu_gauche.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<ul>
+    <li><a href="?cat=pc">PC</a></li>
+    <li><a href="?cat=ps4">PS4</a></li>
+    <li><a href="?cat=xbox">Xbox</a></li>
+    <li><a href="?cat=wii">Wii</a></li>
+</ul>
